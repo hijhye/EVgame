957739abdfc2a343e954c7bb5a0e30a1733391bd
hijhye <yesyuji17@gmail.com>
2025-11-14 16:46:15 +0900

    first

---
+++ js/custom.js
@@ -0,0 +1,105 @@
+// 1. title의 btn 클릭하면 story로 넘어가기
+// 2. story의 btn 클릭하면 game으로 넘어가기
+//3. game의 input.value에 값이 있으면 close버튼 눌렀을 때
+// 3-1. 엘베 문 닫히고 올라가기
+// 3-2.  random수 값 비교해서 결과에 따라 result의 text, img 교체하고, 남은 기회수 카운트해서 star 교체하기
+
+//1.
+let title = document.querySelector(".title");
+let titlebtn = document.querySelector(".title .btn");
+
+titlebtn.addEventListener("click", () => {
+  title.style.display = "none";
+});
+
+//2
+let story = document.querySelector(".story");
+let storybtn = document.querySelector(".story .btn");
+
+storybtn.addEventListener("click", () => {
+  story.style.display = "none";
+});
+
+//3
+random = Math.floor(Math.random() * 50) + 1;
+console.log(random);
+
+let user = document.querySelector(".game input");
+let userNum;
+
+//3-1
+let elevator = document.querySelector(".game");
+let elevatorDoor = document.querySelectorAll(".game .door");
+let close = document.querySelector(".game .close");
+let guide = document.querySelector(".game .textBox p");
+
+close.addEventListener("click", () => {
+  userNum = user.value;
+  if (!userNum == "") {
+    elevatorDoor.forEach((door) => {
+      door.style.width = "50%";
+    });
+    setTimeout(() => {
+      elevator.style.marginTop = "-100vh";
+    }, 1000);
+  } else {
+    guide.textContent = "오른쪽 숫자판에 숫자를 입력하고 닫기버튼을 눌러줘~~";
+  }
+  playGame();
+});
+
+function resetGame() {
+  chances = 5;
+  random = Math.floor(Math.random() * 50) + 1;
+  console.log("새로운 정답:", random);
+  resultStars.textContent = "★".repeat(chances);
+  resultText.textContent = "다시 한 번 출발! 느낌이 오는 층으로 이동해보자!!";
+  isGameOver = false;
+}
+
+//3-2
+let chances = 5;
+let resultStars = document.querySelector(".result .stars");
+let retry = document.querySelector(".result .top .textBox p");
+let retryBtn = document.querySelector(".result .top .btn");
+let resultText = document.querySelector(".resultTxtBox p");
+let isGameOver;
+
+function playGame() {
+  chances--;
+  userNum = user.value;
+  resultStars.textContent = "★".repeat(chances);
+
+  if (userNum === random) {
+    resultText.textContent = "정답이곰~! 우리가 해냈곰!";
+    isGameOver = true;
+  } else if (chances === 0) {
+    retry.textContent = "처음부터 다시 해볼까? ";
+    resultText.textContent = "으앙! 별을 다 잃었곰...";
+    isGameOver = true;
+  } else if (userNum > random) {
+    resultText.textContent = "아까비! 조금 더 아래곰!";
+    setTimeout(() => {
+      guide.textContent = `${userNum}층보다는 아래층이었어! 다시 한 번 시도해보자~!!`;
+    }, 3000);
+    isGameOver = false;
+  } else if (userNum < random) {
+    resultText.textContent = "더 높은 층이곰! Up! Up!";
+    setTimeout(() => {
+      guide.textContent = `${userNum}층보다는 높은 층인가봐! 다시 한 번 시도해보자~!!`;
+    }, 3000);
+    isGameOver = false;
+  }
+
+  user.value = "";
+}
+
+retryBtn.addEventListener("click", () => {
+  if (isGameOver) {
+    resetGame();
+  }
+  elevator.style.marginTop = "0vh";
+  elevatorDoor.forEach((door) => {
+    door.style.width = "0%";
+  });
+});
